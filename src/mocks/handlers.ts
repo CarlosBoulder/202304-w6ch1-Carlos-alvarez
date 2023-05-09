e9f86bc3aa33972ddbb11ceb5d5946060163259b
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-09 20:27:52 +0200

    Add useApi test

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,28 +1,10 @@
 import { rest } from "msw";
+import taskMocks from "./tasksMock";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const handlers = [
   rest.get(apiUrl, (_req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          name: "climb",
-          isDone: false,
-        },
-        {
-          id: 2,
-          name: "eat",
-          isDone: false,
-        },
-        {
-          id: 3,
-          name: "sleep",
-          isDone: false,
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json(taskMocks));
   }),
 ];
---
+++ src/mocks/tasksMock.ts
@@ -0,0 +1,21 @@
+import TasksStructure from "../store/tasks/types";
+
+const taskMocks: TasksStructure[] = [
+  {
+    id: 1,
+    name: "climb",
+    isDone: false,
+  },
+  {
+    id: 2,
+    name: "eat",
+    isDone: false,
+  },
+  {
+    id: 3,
+    name: "sleep",
+    isDone: false,
+  },
+];
+
+export default taskMocks;
