910e339f6993151038b8ff4c8ab39bbed2913913
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-10 18:29:47 +0200

    Create Button component

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -8,9 +8,9 @@ const TaskCard = ({ task: { name, isDone } }: TaskCardProps): JSX.Element => {
   const isDoneForCheckbox: number = isDone ? 1 : 0;
   return (
     <div>
-      <label>
-        <input type="checkbox" value={isDoneForCheckbox} />
+      <label className="container">
         <h3>{name}</h3>
+        <input type="checkbox" value={isDoneForCheckbox} />
       </label>
       <button>Delete</button>
     </div>
